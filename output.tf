e62a67cd611c79738ddbef1cf474eaa6a3b982be
oluwaniyi66 <oluwaniyiropo11@gmail.com>
2023-05-27 16:29:21 +0100

    my first commit

---
+++ output.tf
@@ -0,0 +1,17 @@
+
+output "web_instance_ip" {
+  description = "Web instance complete URL"
+  value = join("", ["http://", aws_instance.cba_tf_instance.public_ip])
+}
+
+output "Time-Date" {
+  description = "Date/Time of Execution"
+  value       = timestamp()
+}
+
+data "aws_region" "current" {}
+
+output "Region" {
+  description = "Region"
+  value       = data.aws_region.current
+}
